abc00142eae2cebcff3c7cc8db8cc54fd59bde55
Daax06 <kokeshi.jm@gmail.com>
2024-06-27 16:19:52 +0800

    yes

--- scheduling.php
+++ scheduling.php
@@ -183,17 +183,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             // Example of available timeslots
             $available_times = [
                 "9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM",
-                "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM"
+                "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "10:00 AM", "1:00 PM", "3:00 PM"
             ];
 
-            // Assume already booked times
-            $booked_times = [
-                "10:00 AM", "1:00 PM", "3:00 PM"
-            ];
 
             // Generate time slots buttons
             foreach ($available_times as $time) {
-                $disabled_class = in_array($time, $booked_times) ? ' disabled' : '';
+                $disabled_class = in_array($time, $booked_times) ? 'disabled' : '';
                 echo '<button class="time-slot' . $disabled_class . '" data-time="' . $time . '">' . $time . '</button>';
             }
             ?>
